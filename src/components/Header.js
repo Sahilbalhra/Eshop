6c2f9cf921124134b257535a16cf1e6bb75ff189
Sahilbalhra <sahilbalhra0@gmail.com>
2022-01-17 13:46:16 +0530

    initial one

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <div className='ui fixed menu'>
+            <div className="ui container center">
+                <h2>EShop(All Data is fetch From api of fakeApiStore)</h2>
+            </div>           
+        </div>
+    )
+}
+export default Header
